7d345abdfb0d33930b0cf91e038e1e2f4ef342f4
Srinivasa Manikanta <srinivasmanikantarajapantula@gmail.com>
2026-01-23 06:40:05 +0530

    Add PWA service worker and update build scripts
    Introduced service worker files for PWA support in the public directory. Updated Next.js build and dev scripts in package.json to use the webpack flag. Minor config adjustment in next.config.ts for PWA plugin options.

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const pwaConfig = withPWA({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  swcMinify: true,
+
   disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: true,
